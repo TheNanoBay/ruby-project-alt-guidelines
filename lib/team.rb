0f08ba6c702a0ed2362f274e4027ad9832f449f4
esmasher <esmasherr@gmail.com>
2021-01-06 11:09:49 -0600

    Completed CRUD functionality

--- lib/team.rb
+++ lib/team.rb
@@ -10,36 +10,40 @@ class Team < ActiveRecord::Base
 
        puts "It's signed! #{signing_player.name} now plays for #{self.name}."
        puts " "
-       new_contract 
+       new_contract
     end
 
-    #read feature - As a user, I want to view all the contracts that were ever signed over time - and their details (Read) 
-    def view_all_contracts_for_team_ever 
+    #read feature - As a user, I want to view all the contracts that were ever signed over time - and their details (Read)
+    def view_all_contracts_for_team_ever
         self.contracts.map do |contract|
             player_name = Player.find(contract.player_id).name
             "#{player_name} signed with #{self.name} for #{contract.total_length} years and it's worth #{contract.total_value} million dollars."
         end
     end
 
-    #read feature - As a user, I want to be able to browse all the current players & all the players signed to a team (Read) 
-    def view_all_players_for_team_ever 
+    #read feature - As a user, I want to be able to browse all the current players & all the players signed to a team (Read)
+    def view_all_players_for_team_ever
         self.players.map do |player|
             "#{player.name} played for the #{self.name} and had the following stats -> Position: #{player.position} Height: #{player.height} Weight: #{player.weight}"
-        end 
+        end
     end
 
-    # Edit feature - As a user, I want to change the terms of a contract (Edit) 
-    def change_contract
-        
+    # Edit feature - As a user, I want to change the terms of a contract (Edit)
+    def change_contract_terms(player_name, new_value, new_length)
+        affected_player = Player.find_by(name: player_name)
+        old_contract = Contract.where(team_id: self.id, player_id: affected_player.id)
+
+        adjusted_contract = old_contract.update(total_value: new_value, total_length: new_length)
+
+        "#{player_name} is now signed with #{self.name} for #{adjusted_contract[0].total_length} years and it's worth #{adjusted_contract[0].total_value} million dollars."
     end
 
     # Delete feature - As a user, I want to delete a contract with a player.. fire them (delete)
-    def break_contract(player_name) 
+    def break_contract(player_name)
         changing_player_id = Player.find_by(name: player_name).id
-        deleted_contract = self.contracts.find(changing_player_id)
+        deleted_contract = self.contracts.find_by(player_id: changing_player_id)
         deleted_contract.destroy
-    end
     
-
-
+        "#{player_name} is now released from the #{self.name}."
+    end
 end
